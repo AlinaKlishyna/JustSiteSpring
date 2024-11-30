c47141ef5bd1cc6fa155feb0254f87cbe84705f1
Alina <alinakotova102@gmail.com>
2024-11-30 13:56:37 +0100

    Initial commit

---
+++ src/main/java/com/gmail/alinakotova102/ServingWebContentApplication.java
@@ -0,0 +1,2 @@
+package com.gmail.alinakotova102;public class ServingWebContentApplication {
+}
---
+++ src/main/java/com/gmail/alinakotova102/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.gmail.alinakotova102;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//програмный модуль
+@Controller
+public class GreetingController {
+
+    //слушает запросы от пользователя и возвращает какие-то данные
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
