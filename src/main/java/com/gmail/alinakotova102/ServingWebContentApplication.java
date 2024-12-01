feb6c7db08c7b21f9907b63d5aa0bd47656a1644
Alina <alinakotova102@gmail.com>
2024-12-01 10:40:31 +0100

    refactored

--- src/main/java/com/gmail/alinakotova102/ServingWebContentApplication.java
+++ src/main/java/com/gmail/alinakotova102/ServingWebContentApplication.java
@@ -2,9 +2,6 @@ package com.gmail.alinakotova102;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
--- src/main/java/com/gmail/alinakotova102/controller/GreetingController.java
+++ src/main/java/com/gmail/alinakotova102/controller/GreetingController.java
@@ -1,12 +1,10 @@
 package com.gmail.alinakotova102.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
-import java.util.Objects;
 
 //програмный модуль
 @Controller
