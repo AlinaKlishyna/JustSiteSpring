64d6eeed1b3a78120bc88b7f54e9825ba075bb57
Alina <alinakotova102@gmail.com>
2024-12-18 23:40:18 +0100

    Spring Boot JPA (Hibernate): add to database and filter

--- src/main/java/com/gmail/alinakotova102/repositories/MessageRepo.java
+++ src/main/java/com/gmail/alinakotova102/repositories/MessageRepo.java
@@ -3,5 +3,9 @@ package com.gmail.alinakotova102.repositories;
 import com.gmail.alinakotova102.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository <Message, Integer> {
+
+    List <Message> findByTag(String tag);
 }
